31e738d70474f7171c9afbbb8601926fd96a9534
Kunalwaghmare12 <kunalwaghmare1222@gmail.com>
2023-12-29 23:48:44 +0530

    this is my second commit

--- index.java
+++ index.java
@@ -1,5 +1,10 @@
 public class index{
     public static void main(String[] args) {
         System.out.println("hello");
+
+        for(int i=0; i<=10;i++){
+            System.out.println("kunal is good boy");
+        }
     }
 }
+
